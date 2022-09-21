180a6139374b486942c6d9b264e1fba8902c6971
Maira Silva <maira.emilly@hotmail.com>
2022-09-21 15:00:07 -0300

    Alg. de Classificação Knn (Parkinsons Data Set)

--- analise_preditiva_knn.R
+++ analise_preditiva_knn.R
@@ -2,20 +2,32 @@
 
 library(ggplot2)
 
-parkinsons <- read.table("C:/Users/Usuário/git-projetos/md-2022-2-mairasilva/dataSets/parkinsons.data", header = TRUE, sep = ",")
-
-parkinsons
-
+parkinsons <- read.table("parkinsons.data", header = TRUE, sep = ",", dec = ".", quote="")
+View(parkinsons)
 
 
-## Utilizar o classificador Knn para classificar o indivíduo phon_R01_S06
+
+## Utilizando o classificador Knn para classificar o indivíduo phon_R01_S06
 
 library(class)
 
-(treinamento <- parkinsons[25:30, 1:23]) 
+## Usando um dataframe sem o indivíduo phon_R01_S06
+df1 <- parkinsons[1:24, ] # pegando a primeira parte (antes do phon_R01_S06)
+df2 <- parkinsons[31:195, ] # pegando a segunda parte (depois do phon_R01_S06)
 
-(rotulos <- parkinsons[25:30, 18:18])
+df3 <- df1
+df3[25:length(df2$name), ] <- df3[1:length(df2$name), ]
+treinamento <- df3
+treinamento <- treinamento[ , -18] # removendo a coluna status
+treinamento <- treinamento[ , 2:23]
 
-teste <- parkinsons[1:1, 2:23]
 
+rotulos <- df3$status # criando o vetor de rotulos - status (one) - Parkinson's, (zero) - healthy
+
+# objeto de teste
+teste <- parkinsons[25:30, ] # indivíduo phon_R01_S06
+teste <- teste[ , -18]
+teste <- teste[ , 2:23]
+
+# chamadando a funcao Knn
 (y_estimado <- knn(treinamento, teste, rotulos, 23))
